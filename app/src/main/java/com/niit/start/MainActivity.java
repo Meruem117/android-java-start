6bdcc64213a70cc1c8f750346b715a2c17fb7d2a
Meruem117 <1572677319@qq.com>
2022-08-29 22:56:47 +0800

    text

--- app/src/main/java/com/niit/start/MainActivity.java
+++ app/src/main/java/com/niit/start/MainActivity.java
@@ -3,6 +3,7 @@ package com.niit.start;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView home_text = findViewById(R.id.home_text);
     }
 }
