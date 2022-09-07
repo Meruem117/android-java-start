74e1bec5bde9c68a18b4ae13e0722380a9c4f7f7
Meruem117 <1572677319@qq.com>
2022-09-07 23:14:19 +0800

    update

--- app/src/main/java/com/niit/start/MainActivity.java
+++ app/src/main/java/com/niit/start/MainActivity.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
