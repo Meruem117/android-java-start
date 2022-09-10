ba63ee9cebcddd9d070b467b76ba5b63a1bd4e61
Meruem117 <1572677319@qq.com>
2022-09-10 23:09:42 +0800

    update

--- app/src/main/java/com/niit/start/HomeActivity.java
+++ app/src/main/java/com/niit/start/HomeActivity.java
@@ -3,12 +3,32 @@ package com.niit.start;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private TextView home_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        home_text = findViewById(R.id.home_text);
+        Button home_button = findViewById(R.id.home_button);
+        home_button.setOnClickListener(new HomeButtonOnClickListener(home_text));
+    }
+
+    static class HomeButtonOnClickListener implements View.OnClickListener {
+        private final TextView home_text;
+
+        public HomeButtonOnClickListener(TextView home_text) {
+            this.home_text = home_text;
+        }
+
+        @Override
+        public void onClick(View view) {
+        }
     }
 }
